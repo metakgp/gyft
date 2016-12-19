6cf1203edfd0667f2f7f05ecb5b2bc6e656d4fdf
Techievena <abinashsena@gmail.com>
2016-12-19 09:34:34 +0530

    Changing the value of dict headers['timeout'] from 20 to '20'.
    Fixes run time error :-
    requests.exceptions.InvalidHeader: Header value 20 must be of type str or bytes, not <type 'int'>

--- gyft.py
+++ gyft.py
@@ -22,7 +22,7 @@ ERP_SECRET_QUESTION_URL = 'https://erp.iitkgp.ernet.in/SSOAdministration/getSecu
 
 
 headers = {
-    'timeout': 20,
+    'timeout': '20',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
 }
 
