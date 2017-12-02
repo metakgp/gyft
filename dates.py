426cceca611b0e148e6646bf70250a407d3d6aaf
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-12-02 23:18:44 +0530

    check the dates input by user for sanity before generating timetable

--- dates.py
+++ dates.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
+
 import build_event
 import datetime
+import pytz
 
-SEM_BEGIN=datetime.datetime.now()
+SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
 #  SEM_BEGIN=build_event.generateIndiaTime(2017, 7, 16, 0, 0)
 
 MID_TERM_BEGIN=build_event.generateIndiaTime(2017, 9, 15, 0, 0)
@@ -9,6 +12,23 @@ MID_TERM_BEGIN=build_event.generateIndiaTime(2017, 9, 15, 0, 0)
 MID_TERM_END=build_event.generateIndiaTime(2017, 9, 25, 23, 59)
 
 END_TERM_BEGIN=build_event.generateIndiaTime(2017, 11, 20, 23, 59)
+
+## Sanity check
+
+sanity = [
+            SEM_BEGIN < MID_TERM_BEGIN,
+            MID_TERM_BEGIN < MID_TERM_END,
+            MID_TERM_END < END_TERM_BEGIN
+         ]
+
+# check if anything is False
+sanity_check = [item for item in sanity if not item]
+
+if len(sanity_check) > 0:
+    print("Check the dates you have entered")
+    print("Note: SEM_BEGIN < MID_TERM_BEGIN < MID_TERM_END < END_TERM_BEGIN")
+    import os
+    os.exit(1)
 
 '''
 Returns a list of lists denoting the time periods of working days
