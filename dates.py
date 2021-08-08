268b484cc62f1eb67c368db1d1112ef411f13ce5
Shivam Kumar Jha <shivam.cs.iit.kgp+github@gmail.com>
2021-08-08 22:15:00 +0530

    Update dates.py
    dates for AUT 2021-22

--- dates.py
+++ dates.py
@@ -6,10 +6,10 @@ import pytz
 import sys
 
 # SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-SEM_BEGIN = build_event.generateIndiaTime(2021, 1, 4, 0, 0)
-MID_TERM_BEGIN = build_event.generateIndiaTime(2021, 2, 27, 0, 0)
-MID_TERM_END = build_event.generateIndiaTime(2021, 3, 3, 23, 59)
-END_TERM_BEGIN = build_event.generateIndiaTime(2021, 4, 14, 0, 0)
+SEM_BEGIN = build_event.generateIndiaTime(2021, 8, 10, 0, 0)
+MID_TERM_BEGIN = build_event.generateIndiaTime(2021, 10, 12, 0, 0)
+MID_TERM_END = build_event.generateIndiaTime(2021, 10, 17, 23, 59)
+END_TERM_BEGIN = build_event.generateIndiaTime(2021, 11, 21, 0, 0)
 
 # Recurrence strings from above dates
 GYFT_RECUR_STRS = [['RRULE:FREQ=WEEKLY;UNTIL={}'.format(END_TERM_BEGIN.strftime('%Y%m%dT000000Z'))],
