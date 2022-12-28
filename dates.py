19fe84b4a3be833a535218ee567dc9931078e249
ashwinpra <ashprasanth2003@gmail.com>
2022-12-28 12:17:06 +0400

    updated dates for spring semester 2022-23

--- dates.py
+++ dates.py
@@ -6,10 +6,10 @@ import pytz
 import sys
 
 # SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-SEM_BEGIN = build_event.generateIndiaTime(2022, 8, 2, 0, 0)
-MID_TERM_BEGIN = build_event.generateIndiaTime(2022, 9, 20, 0, 0)
-MID_TERM_END = build_event.generateIndiaTime(2022, 9, 30, 23, 59)
-END_TERM_BEGIN = build_event.generateIndiaTime(2022, 11, 16, 0, 0)
+SEM_BEGIN = build_event.generateIndiaTime(2023, 1, 3, 0, 0)
+MID_TERM_BEGIN = build_event.generateIndiaTime(2023, 2, 15, 0, 0)
+MID_TERM_END = build_event.generateIndiaTime(2023, 2, 24, 23, 59)
+END_TERM_BEGIN = build_event.generateIndiaTime(2023, 4, 18, 0, 0)
 
 # Recurrence strings from above dates
 GYFT_RECUR_STRS = [['RRULE:FREQ=WEEKLY;UNTIL={}'.format(END_TERM_BEGIN.strftime('%Y%m%dT000000Z'))],
