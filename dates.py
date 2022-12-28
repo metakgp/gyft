535cd9ca14879265f8d8e68863ca1184a9b0fdab
C-12-14 <xe54c13@gmail.com>
2022-12-28 15:17:17 +0530

    Updates dates.py according to the 2023 calendar

--- dates.py
+++ dates.py
@@ -6,24 +6,26 @@ import pytz
 import sys
 
 # SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-SEM_BEGIN = build_event.generateIndiaTime(2022, 8, 2, 0, 0)
-MID_TERM_BEGIN = build_event.generateIndiaTime(2022, 9, 20, 0, 0)
-MID_TERM_END = build_event.generateIndiaTime(2022, 9, 30, 23, 59)
-END_TERM_BEGIN = build_event.generateIndiaTime(2022, 11, 16, 0, 0)
+SEM_BEGIN = build_event.generateIndiaTime(2023, 1, 3, 0, 0)
+MID_TERM_BEGIN = build_event.generateIndiaTime(2023, 2, 15, 0, 0)
+MID_TERM_END = build_event.generateIndiaTime(2023, 2, 24, 23, 59)
+END_TERM_BEGIN = build_event.generateIndiaTime(2023, 4, 18, 0, 0)
 
 # Recurrence strings from above dates
-GYFT_RECUR_STRS = [['RRULE:FREQ=WEEKLY;UNTIL={}'.format(END_TERM_BEGIN.strftime('%Y%m%dT000000Z'))],
-                    ['RRULE:FREQ=WEEKLY;UNTIL={}'.format(MID_TERM_BEGIN.strftime('%Y%m%dT000000Z'))],
-                    ['RRULE:FREQ=WEEKLY;UNTIL={}'.format(END_TERM_BEGIN.strftime('%Y%m%dT070000Z'))],
-                    ['RRULE:FREQ=WEEKLY;UNTIL={}'.format(MID_TERM_BEGIN.strftime('%Y%m%dT080000Z'))]]
+GYFT_RECUR_STRS = [
+    ["RRULE:FREQ=WEEKLY;UNTIL={}".format(END_TERM_BEGIN.strftime("%Y%m%dT000000Z"))],
+    ["RRULE:FREQ=WEEKLY;UNTIL={}".format(MID_TERM_BEGIN.strftime("%Y%m%dT000000Z"))],
+    ["RRULE:FREQ=WEEKLY;UNTIL={}".format(END_TERM_BEGIN.strftime("%Y%m%dT070000Z"))],
+    ["RRULE:FREQ=WEEKLY;UNTIL={}".format(MID_TERM_BEGIN.strftime("%Y%m%dT080000Z"))],
+]
 
 ## Sanity check
 
 sanity = [
-            SEM_BEGIN < MID_TERM_BEGIN,
-            MID_TERM_BEGIN < MID_TERM_END,
-            MID_TERM_END < END_TERM_BEGIN
-         ]
+    SEM_BEGIN < MID_TERM_BEGIN,
+    MID_TERM_BEGIN < MID_TERM_END,
+    MID_TERM_END < END_TERM_BEGIN,
+]
 
 # check if anything is False
 sanity_check = [item for item in sanity if not item]
@@ -33,11 +35,10 @@ if len(sanity_check) > 0:
     print("Note: SEM_BEGIN < MID_TERM_BEGIN < MID_TERM_END < END_TERM_BEGIN")
     sys.exit(1)
 
-'''
+"""
 Returns a list of lists denoting the time periods of working days
-'''
+"""
+
+
 def get_dates():
-    return [
-                [ SEM_BEGIN, MID_TERM_BEGIN ],
-                [ MID_TERM_END, END_TERM_BEGIN ]
-           ]
+    return [[SEM_BEGIN, MID_TERM_BEGIN], [MID_TERM_END, END_TERM_BEGIN]]
