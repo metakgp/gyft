5bf97db09233317c5beeda31a4ea9f10efa35369
ashwinpra <ashprasanth2003@gmail.com>
2023-08-12 00:04:47 +0530

    modularise code; give option to generate ics within gyft.py

--- gyft.py
+++ gyft.py
@@ -3,77 +3,19 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 from bs4 import BeautifulSoup as bs
 import json
+import os.path as osp
 import iitkgp_erp_login.erp as erp
+import argparse
 from dates import SEM_BEGIN
+from generate_ics import generate_ICS
 
 headers = {
     'timeout': '20',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
 }
 
-s = requests.Session()
-
-_, ssoToken = erp.login(headers, s)
-print()
-
-if SEM_BEGIN.month > 6:
-    # autumn semester
-    SEM_NO = (int(SEM_BEGIN.strftime("%y"))-int(erp.ROLL_NUMBER[:2]))*2 + 1
-else:
-    # spring semester
-    SEM_NO = (int(SEM_BEGIN.strftime("%y"))-int(erp.ROLL_NUMBER[:2])) + 2
-
-
 ERP_TIMETABLE_URL = "https://erp.iitkgp.ac.in/Acad/student/view_stud_time_table.jsp"
 COURSES_URL = "https://erp.iitkgp.ac.in/Academic/student_performance_details_ug.htm"
-
-timetable_details = {
-    'ssoToken': ssoToken,
-    'module_id': '16',
-    'menu_id': '40',
-}
-
-coursepage_details = {
-    "ssoToken": ssoToken,
-    "semno": SEM_NO,
-    "rollno": erp.ROLL_NUMBER,
-    "order": "asc"
-}
-
-# This is just a hack to get cookies. TODO: do the standard thing here
-abc = s.post(ERP_TIMETABLE_URL, headers=headers, data=timetable_details)
-cookie_val = None
-for a in s.cookies:
-    if (a.path == "/Acad/"):
-        cookie_val = a.value
-
-cookie = {
-    'JSESSIONID': cookie_val,
-}
-r = s.post(ERP_TIMETABLE_URL, cookies = cookie, headers=headers, data=timetable_details)
-
-soup = bs(r.text, 'html.parser')
-rows_head = soup.findAll('table')[2]
-rows = rows_head.findAll('tr')
-times = []
-
-
-# Delete the rows that doesn't have tableheader, basically without a weekday
-del_rows = []
-for i in range(1, len(rows)):
-    HeaderRows = rows[i].findAll("td", {"class": "tableheader"})
-    if len(HeaderRows) == 0:
-        del_rows.append(i)
-
-for index_del in sorted(del_rows, reverse=True):
-    del rows[index_del]
-
-
-##### For timings
-
-for a in rows[0].findAll('td'):
-    if ('AM' in a.text or 'PM' in a.text):
-        times.append(a.text)
 
 #### For timings end
 days = {}
@@ -86,20 +28,25 @@ days[5] = "Friday"
 days[6] = "Saturday"
 #### For day end
 
-timetable_dict = {}
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input")
+    parser.add_argument("-o", "--output")
+    args = parser.parse_args()
+    return args
 
-for i in range(1, len(rows)):
-    timetable_dict[days[i]] = {}
-    tds = rows[i].findAll('td')
-    time = 0
-    for a in range(1, len(tds)):
-        if not tds[a].find('b'):
-            continue
-        txt = tds[a].find('b').text.strip()
-        if (len(txt) >= 7):
-            timetable_dict[days[i]][times[time]] = list((tds[a].find('b').text[:7],tds[a].find('b').text[7:], int(tds[a]['colspan'])))
-        time = time + int(tds[a]['colspan'])
+def get_cookies(s, timetable_details):
+    # This is just a hack to get cookies. TODO: do the standard thing here
+    abc = s.post(ERP_TIMETABLE_URL, headers=headers, data=timetable_details)
+    cookie_val = None
+    for a in s.cookies:
+        if (a.path == "/Acad/"):
+            cookie_val = a.value
 
+    cookie = {
+        'JSESSIONID': cookie_val,
+    }
+    return cookie
 
 def merge_slots(in_dict):
     for a in in_dict:
@@ -111,38 +58,134 @@ def merge_slots(in_dict):
         in_dict[a] = in_dict[a][0]
     return (in_dict)
 
+def scrape_timetable(s, timetable_details, coursepage_details):
+    cookie = get_cookies(s, timetable_details)
+    r = s.post(ERP_TIMETABLE_URL, cookies = cookie, headers=headers, data=timetable_details)
 
-for day in timetable_dict.keys():
-    subject_timings = {}
-    for time in timetable_dict[day]:
-        flattened_time = int(time[:time.find(':')])
-        if (flattened_time < 6):
-            flattened_time += 12
-        if (not timetable_dict[day][time][0] in subject_timings.keys()):
-            subject_timings[timetable_dict[day][time][0]] = []
-        subject_timings[timetable_dict[day][time][0]].append([flattened_time, timetable_dict[day][time][2]])
-    subject_timings = merge_slots(subject_timings)
-    for time in list(timetable_dict[day].keys()):
-        flattened_time = int(time[:time.find(':')])
-        if (flattened_time < 6):
-            flattened_time += 12
-        if (not flattened_time == subject_timings[timetable_dict[day][time][0]][0]):
-            del (timetable_dict[day][time])
+    soup = bs(r.text, 'html.parser')
+    rows_head = soup.findAll('table')[2]
+    rows = rows_head.findAll('tr')
+    times = []
+
+    # Delete the rows that doesn't have tableheader, basically without a weekday
+    del_rows = []
+    for i in range(1, len(rows)):
+        HeaderRows = rows[i].findAll("td", {"class": "tableheader"})
+        if len(HeaderRows) == 0:
+            del_rows.append(i)
+
+    for index_del in sorted(del_rows, reverse=True):
+        del rows[index_del]
+
+
+    ##### For timings
+    for a in rows[0].findAll('td'):
+        if ('AM' in a.text or 'PM' in a.text):
+            times.append(a.text)
+
+    timetable_dict = {}
+
+    for i in range(1, len(rows)):
+        timetable_dict[days[i]] = {}
+        tds = rows[i].findAll('td')
+        time = 0
+        for a in range(1, len(tds)):
+            if not tds[a].find('b'):
+                continue
+            txt = tds[a].find('b').text.strip()
+            if (len(txt) >= 7):
+                timetable_dict[days[i]][times[time]] = list((tds[a].find('b').text[:7],tds[a].find('b').text[7:], int(tds[a]['colspan'])))
+            time = time + int(tds[a]['colspan'])
+
+    for day in timetable_dict.keys():
+        subject_timings = {}
+        for time in timetable_dict[day]:
+            flattened_time = int(time[:time.find(':')])
+            if (flattened_time < 6):
+                flattened_time += 12
+            if (not timetable_dict[day][time][0] in subject_timings.keys()):
+                subject_timings[timetable_dict[day][time][0]] = []
+            subject_timings[timetable_dict[day][time][0]].append([flattened_time, timetable_dict[day][time][2]])
+        subject_timings = merge_slots(subject_timings)
+        for time in list(timetable_dict[day].keys()):
+            flattened_time = int(time[:time.find(':')])
+            if (flattened_time < 6):
+                flattened_time += 12
+            if (not flattened_time == subject_timings[timetable_dict[day][time][0]][0]):
+                del (timetable_dict[day][time])
+            else:
+                timetable_dict[day][time][2] = subject_timings[timetable_dict[day][time][0]][1]
+
+    # get course names
+    r = s.post(COURSES_URL, headers=headers, data=coursepage_details)
+    sub_dict = {item["subno"]: item["subname"] for item in r.json()}
+    sub_dict = {k: v.replace("&amp;", "&") for k, v in sub_dict.items()} # replacing &amp; with &
+
+    # add course name to dict
+    for day in timetable_dict.keys():
+        for time in timetable_dict[day]:
+            course_code = timetable_dict[day][time][0]
+            timetable_dict[day][time].append(sub_dict[course_code])
+    
+    return timetable_dict
+
+def main():
+    overwrite = True
+    gen_ics = True
+
+    args = parse_args()
+
+    INPUT_FILENAME = args.input if args.input else "data.txt"
+    OUTPUT_FILENAME = "timetable.ics" if args.output is None else args.output
+
+    # check if data.txt exists
+    if osp.exists(INPUT_FILENAME):
+        ow = input(f"Timetable file {INPUT_FILENAME} exists. Do you want to overwrite it? (y/n): ")
+        print()
+        if ow.lower() == 'n':
+            overwrite = False
+
+    if overwrite:
+        s = requests.Session()
+        _, ssoToken = erp.login(headers, s)
+        print()
+
+        if SEM_BEGIN.month > 6:
+            # autumn semester
+            SEM_NO = (int(SEM_BEGIN.strftime("%y"))-int(erp.ROLL_NUMBER[:2]))*2 + 1
         else:
-            timetable_dict[day][time][2] = subject_timings[timetable_dict[day][time][0]][1]
+            # spring semester
+            SEM_NO = (int(SEM_BEGIN.strftime("%y"))-int(erp.ROLL_NUMBER[:2])) + 2
 
-r = s.post(COURSES_URL, headers=headers, data=coursepage_details)
-sub_dict = {item["subno"]: item["subname"] for item in r.json()}
-sub_dict = {k: v.replace("&amp;", "&") for k, v in sub_dict.items()} # replacing &amp; with &
+        timetable_details = {
+            "ssoToken": ssoToken,
+            "module_id": '16',
+            "menu_id": '40',
+        }
+
+        coursepage_details = {
+            "ssoToken": ssoToken,
+            "semno": SEM_NO,
+            "rollno": erp.ROLL_NUMBER,
+            "order": "asc"
+        }
+
+        timetable_dict = scrape_timetable(s, timetable_details, coursepage_details)
+
+        with open(INPUT_FILENAME, 'w') as outfile:
+            json.dump(timetable_dict, outfile, indent = 4, ensure_ascii=False)
+        
+        print(f"\nTimetable saved to {INPUT_FILENAME} file.")
 
 
-# add course name to dict
-for day in timetable_dict.keys():
-    for time in timetable_dict[day]:
-        course_code = timetable_dict[day][time][0]
-        timetable_dict[day][time].append(sub_dict[course_code])
+    gen = input("Would you like to generate an ics file? (y/n): ")
+    print()
+    if gen.lower() == 'n':
+        gen_ics = False
 
-with open('data.txt', 'w') as outfile:
-    json.dump(timetable_dict, outfile, indent = 4, ensure_ascii=False)
+    if gen_ics:
+        generate_ICS(INPUT_FILENAME, OUTPUT_FILENAME)
+        
 
-print ("\nTimetable saved to data.txt file. You can generate the ICS file now by running generate_ics.py.\n")
+if __name__ == "__main__":
+    main()
