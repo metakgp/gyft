1d5d21e5cada973c35584ae8ace8a76943f27422
Rohan Barsagade <rbarsagade101@gmail.com>
2023-09-08 16:39:58 +0530

    Header component

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Header from "../components/Header.svelte";
+</script>
+
+<div id="main">
+  <Header />
+</div>
+
+<style>
+  #main {
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    padding: 1rem;
+    z-index: 20;
+    border: 1px solid white;
+    border-radius: 24px;
+    border: 2px solid #fff;
+    background: rgba(255, 255, 255, 0.3);
+    backdrop-filter: blur(106.5px);
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: space-evenly;
+    gap: 2rem;
+    overflow: auto;
+  }
+</style>
