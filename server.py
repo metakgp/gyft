0c19b8e1b2fef697ca1ed63c4f60f6c39103b8ed
tanush <tanush@myyahoo.com>
2024-06-10 16:14:33 +0530

    jwt security added

--- server.py
+++ server.py
@@ -1,78 +1,83 @@
 import requests
-# from flask import Flask
-from flask import Flask, request,send_file,jsonify
-from timetable import build_courses, generate_ics
+from flask import Flask, request, send_file, jsonify
 import io
-
+import jwt
 from flask_cors import CORS
+from datetime import datetime, timedelta
+from timetable import build_courses, generate_ics
 from session_manager import SessionManager
+from utils import ERPSession
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
+# Initialize the SessionManager instance
+session_manager = SessionManager()
 
-# Start the first cleanup task
-SessionManager.cleanup_sessions()
-
-
+@app.route("/create-session", methods=["POST"])
+def create_session():
+    try:
+        data = request.form
+        roll = data.get("roll")
+        if not roll:
+            return jsonify({"status": "error", "message": "Missing roll number"}), 400
+        
+        token = session_manager.create_session(roll)
+        return jsonify({"status": "success", "token": token}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 @app.route("/secret-question", methods=["POST"])
 def get_secret_question():
     try:
-        data = request.form
-        roll = data.get("roll")
-        if not roll:
-            return jsonify({"status": "error", "message": "Missing roll number"}), 400
+        token = request.headers.get("Authorization")
+        if not token:
+            return jsonify({"status": "error", "message": "Missing token"}), 400
         
-        secret_question = SessionManager.get_secret_question(roll)
+        secret_question = session_manager.get_secret_question(token)
         return jsonify({"status": "success", "secret_question": secret_question}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-
 @app.route("/request-otp", methods=["POST"])
 def request_otp():
     try:
-        data = request.form
-        roll = data.get("roll")
-        passw = data.get("pass")
-        secret_answer = data.get("secret_answer")
+        token = request.headers.get("Authorization")
+        passw = request.form.get("pass")
+        secret_answer = request.form.get("secret_answer")
         
-        if not all([roll, passw, secret_answer]):
-            return jsonify({"status": "error", "message": "Missing roll number, password, or secret answer"}), 400
+        if not all([token, passw, secret_answer]):
+            return jsonify({"status": "error", "message": "Missing token, password, or secret answer"}), 400
         
-        SessionManager.request_otp(roll, passw, secret_answer)
+        session_manager.request_otp(token, passw, secret_answer)
         return jsonify({"status": "success", "message": "OTP sent"}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-
 @app.route("/login", methods=["POST"])
 def login_and_download_ics():
     try:
-        data = request.form
-        roll = data.get("roll")
-        email_otp = data.get("email_otp")
+        token = request.headers.get("Authorization")
+        email_otp = request.form.get("email_otp")
         
-        if not all([roll, email_otp]):
-            return jsonify({"status": "error", "message": "Missing roll number or email OTP"}), 400
+        if not all([token, email_otp]):
+            return jsonify({"status": "error", "message": "Missing token or email OTP"}), 400
         
-        SessionManager.establish_erp_session(roll, email_otp)
+        session_manager.establish_erp_session(token, email_otp)
         return jsonify({"status": "success"}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-
 @app.route("/download-ics", methods=["POST"])
 def download_ics():
     try:
-        data = request.form
-        roll = data.get("roll")
+        token = request.headers.get("Authorization")
         
-        if not roll:
-            return jsonify({"status": "error", "message": "Missing roll number"}), 400
+        if not token:
+            return jsonify({"status": "error", "message": "Missing token"}), 400
         
-        erp_session = SessionManager.get_erp_session(roll)
+        session = session_manager.get_erp_session(token)
+        erp_session = ERPSession.create_erp_session(session.requests_session, session.sso_token, session.login_details.get("user_id"))
         
         timetable_page = erp_session.post(erp_session.ERP_TIMETABLE_URL, cookies=True,
                                           data=erp_session.get_timetable_details())
@@ -89,14 +94,12 @@ def download_ics():
         return send_file(
             ics_file,
             as_attachment=True,
-           
             mimetype='text/calendar',
-            download_name=f"{roll}_timetable.ics"
-)
+            download_name=f"{token}_timetable.ics"
+        )
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-if __name__ == '__main__': 
-    # Run the application on the local development server 
+if __name__ == '__main__':
+    # Run the application on the local development server
     app.run()
-
--- session_manager.py
+++ session_manager.py
@@ -1,86 +1,116 @@
-import iitkgp_erp_login.erp as erp
-import requests
-from utils import ERPSession
+import jwt
+import os
+import sys
 from typing import Dict
 from datetime import datetime, timedelta
 from threading import Timer
-headers = {
-   'timeout': '20',
-   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
-}
-
-
+from iitkgp_erp_login import erp
+import requests
+# Constants
+SECRET_KEY = 'your_secret_key'  # Use a secure secret key for JWT
 
 class Session:
-    def __init__(self, session_token: str ="", sso_token: str ="", erp_session: ERPSession = None, login_details: Dict[str, str] = None):
-        self.session_token = session_token
-        self.sso_token = sso_token
-        self.requests_session = requests.Session()
-        self.erp_session = erp_session
-        self.login_details = login_details
-        self.timestamp = datetime.now() 
-
+    def __init__(self):
+        self.requests_session =  requests.Session()  # Initialize this as per your actual Session implementation
+        self.session_token = None
+        self.login_details = None
+        self.sso_token = None
+        
+        self.timestamp = datetime.now()
 
 class SessionManager:
-    sessions: Dict[str, Session] = {}
+    _instance = None
+    sessions: Dict[str, Session]
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(SessionManager, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
-    @staticmethod
-    def get_secret_question(roll):
-        session = SessionManager.sessions.get(roll)
+    def __init__(self, headers: Dict[str, str] = None):
+        if not hasattr(self, 'initialized'):  # To ensure __init__ is called only once
+            self.sessions = {}
+            self.headers = headers or {
+                'timeout': '20',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36'
+            }
+            self.initialized = True
+            self.cleanup_sessions()
+
+    def generate_jwt(self, roll):
+        payload = {
+            'roll': roll,
+            'exp': datetime.utcnow() + timedelta(minutes=30)  # Token expiration time
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return token
+
+    def verify_jwt(self, token):
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            return payload['roll']
+        except jwt.ExpiredSignatureError:
+            raise ValueError("Token has expired")
+        except jwt.InvalidTokenError:
+            raise ValueError("Invalid token")
+
+    def create_session(self, roll):
+        token = self.generate_jwt(roll)
+        session = Session()
+        self.sessions[token] = session
+        return token
+
+    def get_secret_question(self, token):
+        roll = self.verify_jwt(token)
+        session = self.sessions.get(token)
         if not session:
             session = Session()
-            SessionManager.sessions[roll] = session
+            self.sessions[token] = session
         
         session_token = erp.get_sessiontoken(session.requests_session)
         session.session_token = session_token
-        secret_question = erp.get_secret_question(headers, session.requests_session, roll)
+        secret_question = erp.get_secret_question(self.headers, session.requests_session, roll)
         return secret_question
 
-    @staticmethod
-    def request_otp(roll, passw, secret_answer):
-        session = SessionManager.sessions.get(roll)
+    def request_otp(self, token, passw, secret_answer):
+        roll = self.verify_jwt(token)
+        session = self.sessions.get(token)
         if not session:
-            raise ValueError(f"No session found for roll number {roll}")
-        
+            raise ValueError(f"No session found for token")
+
         login_details = erp.get_login_details(roll, passw, secret_answer, session.session_token)
+        
         session.login_details = login_details
-        erp.request_otp(headers, session.requests_session, login_details)
+        erp.request_otp(self.headers, session.requests_session, login_details)
 
-    @staticmethod
-    def establish_erp_session(roll, email_otp):
-        session = SessionManager.sessions.get(roll)
+    def establish_erp_session(self, token, email_otp):
+        roll = self.verify_jwt(token)
+        session = self.sessions.get(token)
         if not session or not session.login_details:
-            raise ValueError(f"No login details found for roll number {roll}")
-        
+            raise ValueError(f"No login details found for token")
+
         session.login_details["email_otp"] = email_otp
-        sso_token = erp.signin(headers, session.requests_session, session.login_details)
+        sso_token = erp.signin(self.headers, session.requests_session, session.login_details)
         session.sso_token = sso_token
-        erp_session = ERPSession.create_erp_session(session.requests_session, sso_token, roll)
-        session.erp_session = erp_session
-        return erp_session
-     
-    @staticmethod
-    def get_erp_session(roll):
-        session = SessionManager.sessions.get(roll)
-        if not session or not session.erp_session:
-            raise ValueError(f"No ERP session found for roll number {roll}")
+        return session
+
+    def get_erp_session(self, token):
+        roll = self.verify_jwt(token)
+        session = self.sessions.get(token)
+        if not session: 
+            raise ValueError(f"No ERP session found for token")
         
-        return session.erp_session
+        return session
 
-    @staticmethod
-    def end_session(roll):
-        if roll in SessionManager.sessions:
-            del SessionManager.sessions[roll]
+    def end_session(self, token):
+        if token in self.sessions:
+            del self.sessions[token]
         return "Session ended"
-    
-    @staticmethod
-    def cleanup_sessions():
+
+    def cleanup_sessions(self):
         now = datetime.now()
-        expired_sessions = [roll for roll, session in SessionManager.sessions.items()
-                            if now - session.timestamp > timedelta(minutes=5)]
-        for roll in expired_sessions:
-            del SessionManager.sessions[roll]
-        # Schedule the next cleanup in 5 minutes
-        Timer(300, SessionManager.cleanup_sessions).start()
-
-
+        expired_sessions = [token for token, session in self.sessions.items()
+                            if now - session.timestamp > timedelta(minutes=10)]
+        for token in expired_sessions:
+            del self.sessions[token]
+        # Schedule the next cleanup in 10 minutes
+        Timer(600, self.cleanup_sessions).start()
