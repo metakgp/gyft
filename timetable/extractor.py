23ef5f83ae7a020e83195104c0ae9b9f5fe48cf9
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-08-01 20:50:13 +0530

    refactor: cleaned up build_courses fix: added empty cell check before code check

--- timetable/extractor.py
+++ timetable/extractor.py
@@ -80,51 +80,59 @@ def build_courses(html: str, course_names: dict) -> list[Course]:
             continue
         day = days[day_cell.get_text()] if day_cell.get_text() in days.keys() else day_cell.get_text()
 
-        # Merge timeslots occurring adjacent to each other and initialize course objects
+        # This is the previously parsed course/time slot. Used to merge timeslots occurring adjacent to each other and initialize course objects
         prev: Course | None = None
-        course_duration: int = 0
         cells = [cell for cell in row.find_all('td') if cell.attrs.get('valign') != 'top']
 
         for index, cell in enumerate(cells):
-            code = cell.get_text()[:7].strip() if cell.get_text()[:7] != "CS10001" else "CS10003"
+            # Empty cell means either previous class just ended or no class is scheduled
+            if cell.get_text().strip() == "":
+                if prev:
+                    # Previous slot ended
+                    courses.append(prev)
+                    prev = None
+                continue
+
+            code = cell.get_text()[:7].strip()
             if not code: continue   # continue if cell has no course in it
-            # CS10003 is the actual code, but it is written as CS10001 in the timetable
+
+            # Special exception: CS10003 is the actual code, but it is written as CS10001 in the timetable
+            if code == "CS10001":
+                code = "CS10003"
+
             location = cell.get_text()[7:]
             cell_duration = int(cell.attrs.get('colspan'))
-            # To reuse code, uses outer scope variables
-            def append_prev():
-                prev.duration = course_duration
-                courses.append(prev)
-
-            # Empty cell means either previous class just ended or no class is scheduled
-            if cell.get_text() == ' ' or cell.get_text() == ' ':
-                if prev:
-                    append_prev()
-                course_duration = 0
-                prev = None
-                continue
 
             # Either new class started just after previous one or previous class is continued
             if prev:
                 # If previous class is same as current class, add the duration to the previous class
                 if code == prev.code:
-                    course_duration += cell_duration
+                    prev.duration += cell_duration
                 # The previous class is different from the current one, meaning previous class ended. Clean up, add to
                 # list and initialize a new course object for the current class
                 else:
-                    append_prev()
-                    prev = Course(code=code, name=course_names[code], day=day,
-                                  start_time=timings[index],
-                                  location=location)
-                    course_duration = cell_duration
+                    courses.append(prev)
+                    prev = Course(
+                        code=code,
+                        name=course_names[code],
+                        day=day,
+                        start_time=timings[index],
+                        location=location,
+                        duration=cell_duration
+                    )
             # New class started after break
             else:
-                prev = Course(code=code, name=course_names[code], day=day,
-                              start_time=timings[index],
-                              location=location)
-                course_duration = cell_duration
+                prev = Course(
+                    code=code,
+                    name=course_names[code],
+                    day=day,
+                    start_time=timings[index],
+                    location=location,
+                    duration=cell_duration
+                )
 
             # Day ended, add the last class
             if index == len(cells) - 1:
-                append_prev()
+                courses.append(prev)
+                prev = None
     return courses
